1518f13fa2d37b4dfd22197a1c4e398c23ef73cd
ufdilla <learnmore.cumi@gmail.com>
2017-11-23 11:47:59 +0700

    beautify code

--- src/recurs/Recurs.java
+++ src/recurs/Recurs.java
@@ -1,36 +1,30 @@
-
 package recurs;
 
 import java.util.*;
 
 public class Recurs 
 {
-    static int i = 1;
-    public static void main (String [] args)
-    {
-        Scanner var = new Scanner (System.in);
-        System.out.print ("Masukkan jumlah = ");
-        int n = var.nextInt ();
-        acak (n, 'A', 'B', 'C');
-    }
-    
-    public static void acak (int n, char Asal, char Bantu, char Tujuan)
-    {
-        try 
-                {
-                   if (n != 0)
-        {
-            acak(n-1, Asal, Tujuan, Bantu);
-            System.out.println("Urutan ke "+ i++ +", acak dari "+ Asal+" ke "+Tujuan);
-            acak (n-1, Bantu, Asal, Tujuan);
-            
-        } 
-                }
-                 
-                catch (RuntimeException err) 
-                        {
-                            err.getMessage();
-                        }
-        
-}
+ static int i = 1;
+ public static void main(String[] args) {
+  Scanner
+  var = new Scanner(System.in);
+  System.out.print("Masukkan jumlah = ");
+  int n =
+   var.nextInt();
+  acak(n, 'A', 'B');
+ }
+
+ public static void acak(int n, char Asal, char Tujuan) {
+  try {
+   if (n != 0) {
+    acak(n - 1, Asal, Tujuan);
+    System.out.println("Urutan ke " + i++ + ", acak dari " + Asal + " ke " + Tujuan);
+    acak(n - 1, Asal, Tujuan);
+
+   }
+  } catch (RuntimeException err) {
+   err.getMessage();
+  }
+ }
+ 
 }
